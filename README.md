f548e717cd6dfaac9013324f244f8676434c208d
Reha Sterbin <reha@omniti.com>
2022-03-01 14:11:40 -0500

    First draft

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # parse-axios-error
+
 Personal use function for parsing axios errors
+
+## How to use
+
+with promises:
+
+```
+  myAxios.get('/widgets/' + widgetId)
+    .then((res) => {
+      const err = parseAxiosResponseForError(res, myParseConfig);
+      if (err.ok) {
+        // do stuff on res
+      } else {
+        // do stuff on err (err.code contains the code, err.message contains the
+        //   message, err.context contains context details)
+      }
+    })
+    .catch((e) => {
+      const err = parseAxiosError(e, parseConfig);
+      // do stuff on err (err.code contains the code, err.message contains the
+      //   message, err.context contains context details)
+    })
+```
+
+with async/await:
+
+```
+  var res = null;
+  var err = null;
+  try {
+    res = await myAxios.get('/widgets/' + widgetId);
+    err = parseAxiosResponseForError(res, myParseConfig);
+  } catch (e) {
+    err = parseAxiosError(e, parseConfig);
+  }
+  if (err.ok) {
+    // do stuff on res
+  } else {
+    // do stuff on err (err.code contains the code, err.message contains the
+    //   message, err.context contains context details)
+  }
+```
+
+## Return spec
+
+key | type | description
+-------------------------
+`ok` | boolean | whether this is a success (true) or error (false)
+`code` | string | the error code (can be NON_AXIOS_ERROR, NETWORK_FAILURE, UNKNOWN, along with any other codes defined by your app)
+`context` | object | contextual information about the request/response (see below)
+
+### Context
+
+key | type | description
+-------------------------
+`error` | Error | if code is NON_AXIOS_ERROR, the error found during processing
+`message` | string | the error message Axios reported
+`status` | int | the HTTP status, if present
+`request_url` | string | the URL for the request
+`request_data` | object | any json data passed in the request
+
+## Options
+
+key | type | default | description
+----------------------------------
+`throwOnNonAxios` | boolean | `false` | if it's a non-axios error, throw an exception (otherwise returns code NON_AXIOS_ERROR)
+`isReponseError` | function | `(data) => data.ok` | if we have data from the server, return whether it's an error
+`findErrorCode` | function | `(data) => data.code` | if we have data from the server, return an error code
+`findErrorMessage` | function | `(data) => data.message` | if we have data from the server, return an error message
+`findErrorContext` | function | `(data, context) => context` | if we have data from the server, take the json data and the context object we have so far and return a final version of the context object
+`scrubRequestUrl` | function | `(url) => url` | scrub any private data from the request URL before returning as context
+`scrubRequestData` | function | `(data) => data` | scrub any private data from the request data json before returning as context
+
---
+++ index.js
@@ -0,0 +1,106 @@
+import axios from 'axios';
+
+// The structure of the parseAxiosError function is based on this excellent
+// StackOverflow answer by Shubham Khatri: https://stackoverflow.com/a/44806462
+
+const DefaultOptions = {
+  throwOnNonAxios: false,
+  isReponseError: (data) => data.ok,
+  findErrorCode: (data) => data.code,
+  findErrorMessage: (data) => data.message,
+  findErrorContext: (data, context) => context,
+  scrubRequestUrl: (url) => url,
+  scrubRequestData: (data) => data,
+};
+
+export const contextFromResponse = (response, opt = {}) => {
+  const options = { ...DefaultOptions, opt };
+  return = {
+    status: response.status,
+    message: null,
+    request_url: options.scrubRequestUrl(response.config.baseURL + response.config.url),
+    request_data: options.scrubRequestData(response.config.data)
+  };
+};
+
+export const parseAxiosResponseForError = (response, opt = {}) => {
+
+  const options = { ...DefaultOptions, opt };
+
+  const context: {
+    status: response.status,
+    message: null,
+    request_url: options.scrubRequestUrl(response.config.baseURL + response.config.url),
+    request_data: options.scrubRequestData(response.config.data)
+  };
+
+  return {
+    ok: options.isReponseSuccess(response.data),
+    code: options.findErrorCode(response.data),
+    message: options.findErrorMessage(response.data),
+    context: options.findErrorContext(response.data, context)
+  }
+};
+
+export default const parseAxiosError = (error, opt = {}) => {
+
+  const options = { ...DefaultOptions, opt };
+
+  if (!error.isAxiosError) {
+    if (options.throwOnNonAxios) {
+      throw error;
+    } else {
+      return {
+        ok: false,
+        code: 'NON_AXIOS_ERROR',
+        message: error.message,
+        context: { error: error },
+      };
+    }
+  }
+
+  let context = contextFromError(error, options);
+  const info = error.toJSON();
+  let context = {
+    status: null,
+    message: info.message,
+    request_url: options.scrubRequestUrl(info.config.baseURL),
+    request_data: options.scrubRequestData(info.config.data)
+  };
+
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    context.status = error.response.status;
+    context.request_url = config.request_url + options.scrubRequestUrl(error.response.config.url);
+    return {
+      ok: false,
+      code: options.findErrorCode(error.response.data),
+      message: options.findErrorMessage(error.response.data),
+      context: options.findErrorContext(error.response.data, context)
+    };
+
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    return {
+      ok: false,
+      code: 'NETWORK_FAILURE',
+      message: context.message,
+      context: context
+    };
+
+  } else {
+    // Something else happened in setting up the request that triggered an error
+    return {
+      ok: false,
+      code: 'UNKNOWN',
+      message: error.message,
+      context: context
+    };
+
+  }
+};
+
+export default parseAxiosError;
